a96192fba59100e62b32523e718f9e118987de48
mostafiz-cse <mdmrmanik1000@gmail.com>
2026-02-27 21:34:07 +0600

    update database connection and warehouse service improvements

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.21
 
 require github.com/golang-jwt/jwt/v5 v5.3.1
 
-require github.com/lib/pq v1.11.2 // indirect
+require github.com/lib/pq v1.11.2
--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -11,7 +11,7 @@ import (
 var DB *sql.DB
 
 func Connect() {
-	connStr := "host=localhost port=5432 user=postgres password=352235 dbname=inventory_db sslmode=disable"
+	connStr := "host=localhost port=5432 user=postgres password=1234 dbname=inventory_db sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
